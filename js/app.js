12572d2da2a590de8814ec66442e0542f7484f15
verooroaltamirano <srtaoro@gmail.com>
2021-10-19 21:04:38 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).ready (function(){ 
+
+    $("#boton-electro").click(function(){
+        $(".electro,.cocina,.hardware,.belleza,.arte,.mascotas").show()
+        $(".electro,.arte,.mascotas").toggle(1000)
+    })
+    
+    $("#boton-cocina").click(function(){
+        $(".electro,.cocina,.hardware,.belleza,.arte,.mascotas").show()
+        $(".hardware,.electro,.mascotas").toggle(1000)
+    })
+
+    $("#boton-hardware").click(function(){
+        $(".electro,.cocina,.hardware,.belleza,.arte,.mascotas").show()
+        $(".electro,.hardware,.cocina").toggle(1000)
+        console.log("boton hardware")
+    })
+    $("#boton-belleza").click(function () {
+        $(".electro,.cocina,.hardware,.belleza,.arte,.mascotas").show()
+        $(".hardware,.cocina,.mascotas").toggle(1000)
+        console.log("boton belleza")
+    })
+    $("#boton-arte").click(function () {
+        $(".cocina,.electro,.hardware,.belleza,.arte,.mascotas").show()
+        $(".belleza,.cocina,.electro").toggle(1000)
+        console.log("boton arte")
+    })
+    $("#boton-mascotas").click(function () {
+        $(".cocina,.electro,.hardware,.belleza,.arte,.mascotas").show()
+        $(".hardware,.cocina,.electro").toggle(1000)
+        console.log("boton mascotas")
+
+    })
+
+});
