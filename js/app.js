b9003eabd2a2d1b0cbbf74b534d339c8b50acc33
verooroaltamirano <srtaoro@gmail.com>
2021-10-27 01:44:32 -0300

    cambioultimo

--- js/app.js
+++ js/app.js
@@ -1,35 +1,40 @@
 $(document).ready (function(){ 
 
     $("#boton-electro").click(function(){
-        $(".electro,.cocina,.hardware,.belleza,.arte,.mascotas").show()
-        $(".electro,.arte,.mascotas").toggle(1000)
+        $(".electro,.cocina,.Hardware,.belleza,.arte,.mascotas").show()
+        $(".cocina,.Hardware,.belleza,.arte,.mascotas").toggle(1000)
     })
     
     $("#boton-cocina").click(function(){
-        $(".electro,.cocina,.hardware,.belleza,.arte,.mascotas").show()
-        $(".hardware,.electro,.mascotas").toggle(1000)
+        $(".electro,.cocina,.Hardware,.belleza,.arte,.mascotas").show()
+        $(".electro,.Hardware,.belleza,.arte,.mascotas").toggle(1000)
     })
 
     $("#boton-hardware").click(function(){
-        $(".electro,.cocina,.hardware,.belleza,.arte,.mascotas").show()
-        $(".electro,.hardware,.cocina").toggle(1000)
+        $(".electro,.cocina,.Hardware,.belleza,.arte,.mascotas").show()
+        $(".electro,.cocina,.belleza,.arte,.mascotas").toggle(1000)
         console.log("boton hardware")
     })
     $("#boton-belleza").click(function () {
-        $(".electro,.cocina,.hardware,.belleza,.arte,.mascotas").show()
-        $(".hardware,.cocina,.mascotas").toggle(1000)
+        $(".electro,.cocina,.Hardware,.belleza,.arte,.mascotas").show()
+        $(".electro,.cocina,.Hardware,.arte,.mascotas").toggle(1000)
         console.log("boton belleza")
     })
     $("#boton-arte").click(function () {
-        $(".cocina,.electro,.hardware,.belleza,.arte,.mascotas").show()
-        $(".belleza,.cocina,.electro").toggle(1000)
+        $(".cocina,.electro,.Hardware,.belleza,.arte,.mascotas").show()
+        $(".electro,.cocina,.Hardware,.belleza,.mascotas").toggle(1000)
         console.log("boton arte")
     })
     $("#boton-mascotas").click(function () {
-        $(".cocina,.electro,.hardware,.belleza,.arte,.mascotas").show()
-        $(".hardware,.cocina,.electro").toggle(1000)
+        $(".cocina,.electro,.Hardware,.belleza,.arte,.mascotas").show()
+        $(".electro,.cocina,.Hardware,.belleza,.arte").toggle(1000)
         console.log("boton mascotas")
 
+    })
+    $("#boton-todos").click(function () {
+        $(".cocina,.electro,.Hardware,.belleza,.arte,.mascotas").show()
+        $(".electro,.cocina,.Hardware,.belleza,.arte,.mascotas").toggle(1100)
+        console.log("boton todos")
     })
 
 });
